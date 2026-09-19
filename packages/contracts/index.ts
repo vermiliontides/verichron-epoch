ef5f2dbc65b6bcb0f5433cb50d40e7c94996f094
vermiliontides <vermiliontides@gmail.com>
2026-09-19 02:55:48 -0500

    refactor: style updates to epoch-202 and updates to contracts

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -13,7 +13,9 @@
  * directory.
  */
 
-export { NormalizedRecord, SourceType } from "./normalizedRecord.js";
+export { NormalizedRecord } from "./normalizedRecord.js";
 export { discoverBackups, BACKUP_SEARCH_MAX_DEPTH } from "./discoverBackups.js";
 export type { Backup } from "./discoverBackups.js";
 export { deriveResultsPath } from './deriveResultsPath.js';
+export { SourceType, isSourceType } from './ts/sourceType'
+export type { SourceType as SourceTypeValue } from './ts/sourceType'
